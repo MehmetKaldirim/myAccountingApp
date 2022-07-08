4fc846ac13372c2c9821ce05dc2d39ebf4132ef6
Mehmet <mathdenizi@gmail.com>
2022-07-08 12:06:01 +0200

    minor change in invoice

--- src/main/java/com/zeroToHero/accountingapp/repository/InvoiceRepository.java
+++ src/main/java/com/zeroToHero/accountingapp/repository/InvoiceRepository.java
@@ -20,21 +20,6 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     List<Invoice> findAllByInvoiceTypeAndCompany(InvoiceType invoiceType, Company company);
 
-
-    //List<Invoice>findInvoiceByInvoiceDateChronology();
-
-    //SELECT billing_date,
-    //FROM collection
-    //ORDER BY billing_date DESC;
-
-
-    //@Query(value = "SELECT * FROM cinema c JOIN location l " +
-      //      "ON l.id = c.location_id WHERE l.country = ?1", nativeQuery = true)
-    //List<Cinema> retrieveAllLocationCountry(@Param("locationCountry") String locationCountry);
-
-    //SELECT * FROM  customer INNER JOIN payment ON payment.customer_id = customer.customer_id ORDER BY payment_date;
-    //SELECT due_date FROM collection ORDER BY due_date ASC;
-
     @Query(value = "SELECT * FROM invoice i INNER JOIN company c ON c.id = i.company_id WHERE c.title = ?1 ORDER BY i.invoice_date DESC LIMIT 3 ", nativeQuery = true)
     List<Invoice> findInvoice(@Param("companyTitle") String companyTitle);
 }
