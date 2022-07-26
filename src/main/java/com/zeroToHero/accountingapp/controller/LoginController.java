cc9d55496807737240ebb5727df06abec8e9d9c8
Mehmet <mathdenizi@gmail.com>
2022-07-26 05:00:26 +0200

    data sql bugs cleaned

--- src/main/java/com/zeroToHero/accountingapp/controller/LoginController.java
+++ src/main/java/com/zeroToHero/accountingapp/controller/LoginController.java
@@ -1,20 +1,29 @@
 package com.zeroToHero.accountingapp.controller;
 
+import com.zeroToHero.accountingapp.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class LoginController {
 
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping(value = {"/", "/login"})
     public String login() {
+
         return "login";
     }
 
     @RequestMapping("/main2")
-    public String main() {
+    public String main(Model model) {
         return "main2";
     }
 
